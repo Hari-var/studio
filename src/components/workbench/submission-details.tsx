c56fb502832c89dbe691174d9bc09adcef0ca14b
Hari Subhash <subhashhari407@gmail.com>
2025-09-26 08:00:46 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/workbench/submission-details.tsx
+++ src/components/workbench/submission-details.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { Submission } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Edit, Trash2, ExternalLink, ChevronsUpDown, ChevronDown } from "lucide-react";
